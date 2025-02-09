4d491af017ad2888faadc6718bfdf66c927b3226
BadBoyBarry <100185817@student.burnley.ac.uk>
2025-02-09 22:13:01 +0000

    this is so tuff

---
+++ LOGBOOK/WEEK1/TASK4.java
@@ -0,0 +1,16 @@
+package WEEK1;
+
+import java.util.Scanner;
+
+public class TASK4
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int N1 = sc.nextInt();
+        System.out.print("Enter the second number:");
+        int N2 = sc.nextInt();
+        System.out.println((N1+N2)/2);
+    }
+}
---
+++ LOGBOOK/WEEK1/TASK8.java
@@ -0,0 +1,15 @@
+package WEEK1;
+
+import java.util.Scanner;
+
+public class TASK8
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of feet: ");
+        double feet = sc.nextDouble();
+        double miles = feet / 5280;
+        System.out.println(feet + " feet is " + miles + " miles.");
+    }
+}
