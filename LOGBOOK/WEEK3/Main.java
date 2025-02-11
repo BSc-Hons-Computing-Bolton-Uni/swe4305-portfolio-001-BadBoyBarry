01e63171fb9df938fbbd5d53dd156550c479b936
BadBoyBarry <100185817@student.burnley.ac.uk>
2025-02-11 20:31:01 +0000

    this is so tuff

---
+++ LOGBOOK/WEEK3/Main.java
@@ -0,0 +1,13 @@
+package WEEK3;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Student stewident = new Student(3,"bleepleshart");
+        stewident.printVal();
+
+        Course Coursident = new Course("bleepleglish", "bleepleshart");
+        Coursident.printVal();
+    }
+}
